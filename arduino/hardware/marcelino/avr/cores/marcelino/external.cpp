c684ee8a9314e22f8504e5800ed5746ccea5db9c
Marcelo H Moraes <marceloh220@hotmail.com>
2017-03-12 12:08:05 -0300

    Marcelino Core to Arduino IDE

--- arduino/hardware/marcelino/avr/cores/marcelino/external.cpp
+++ arduino/hardware/marcelino/avr/cores/marcelino/external.cpp
@@ -1,3 +1,35 @@
+/*
+  Copyright (c) 2017 Marcelo Henrique Moraes
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3.0 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+ */
+
+
+/*
+  Copyright (c) 2017 Marcelo Henrique Moraes
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3.0 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+ */
+
+
 #include "external.h"
 
 VoidFuncPtr INTArray[2] = {none,none};
@@ -64,6 +96,65 @@ void External::detachINT(uint8_t interrupt) {
 	
 }
 
+uint8_t External::rising(uint8_t interrupt) {
+	
+	uint8_t aux = EICRA;
+	
+	if(interrupt) {
+		aux&=(3<<ISC10);
+		aux=aux>>ISC10;
+	}
+	else
+		aux&=(3<<ISC00);
+		
+	return aux == 3 ? 1 : 0;
+	
+}
+
+uint8_t External::falling(uint8_t interrupt) {
+	
+		uint8_t aux = EICRA;
+	
+	if(interrupt) {
+		aux&=(3<<ISC10);
+		aux=aux>>ISC10;
+	}
+	else
+		aux&=(3<<ISC00);
+		
+	return aux == 2 ? 1 : 0;
+	
+}
+
+uint8_t External::change(uint8_t interrupt) {
+	
+		uint8_t aux = EICRA;
+	
+	if(interrupt) {
+		aux&=(3<<ISC10);
+		aux=aux>>ISC10;
+	}
+	else
+		aux&=(3<<ISC00);
+		
+	return aux == 1 ? 1 : 0;
+	
+}
+
+uint8_t External::lower(uint8_t interrupt) {
+	
+		uint8_t aux = EICRA;
+	
+	if(interrupt) {
+		aux&=(3<<ISC10);
+		aux=aux>>ISC10;
+	}
+	else
+		aux&=(3<<ISC00);
+		
+	return !aux;
+	
+}
 
 ISR(INT0_vect) {
 	sregSAVE = SREG;
--- arduino/hardware/marcelino/avr/cores/marcelino/serial.cpp
+++ arduino/hardware/marcelino/avr/cores/marcelino/serial.cpp
@@ -1,3 +1,19 @@
+ /*
+  Copyright (c) 2017 Marcelo Henrique Moraes
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3.0 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+ */
+
+
 #include "serial.h"
 
 Serial::Serial(uint16_t rate) {
@@ -14,6 +30,7 @@ void Serial::mode(uint8_t _mode) {
 		if(_mode) {
 			XCLK_DDR|=(1<<XCLK_PIN);
 			test_mode = 1;
+		}
 }
 
 void Serial::bits(uint8_t bit) {
