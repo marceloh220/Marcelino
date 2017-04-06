dd97bbd8e05492414d3ffbb38dddae3166c4ac33
Marcelo H Moraes <marceloh220@hotmail.com>
2017-04-05 22:47:32 -0300

    Timer2 module with asynchronous mode
    Timer2 module now has asynchronous mode selectable in attach method.
    More information in Timer2 module manual.
    Example to RTC with ATmega stand alone chip with a 32.768KHz crystal oscillator.

---
+++ arduino/hardware/marcelino/avr/core/cmath.cpp
@@ -0,0 +1,52 @@
+/*
+  Copyright (c) 2017 Marcelo Henrique Moraes
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3.0 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+ */
+
+#include "cmath.h"
+
+float Math::euler(float x) {
+	return (float)::pow(EULER,x);
+}
+
+float Math::sqrt(float x, float y) {
+	return (float)::pow(x,1/y);
+}
+
+float Math::log(float x, float base) {
+	return (float)(log10(x)/log10(base));
+}
+
+long Math::random(long big) {
+	if (big == 0)
+		return 0;
+	return rand() % big;
+}
+
+long Math::random(long small, long big) {
+	if (small >= big)
+		return small;
+	return random(big - small) + small;
+}
+
+long Math::tobcd(long _dec) {
+	return ( ( (_dec / 10) * 16 ) + (_dec % 10) );	
+}
+
+long Math::todec(long _bcd) {
+	return ((_bcd & 0xF0) >> 4) * 10 + (_bcd & 0x0F);
+}
+
+long Math::map(long x, long in_min, long in_max, long out_min, long out_max) {
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
---
+++ arduino/hardware/marcelino/avr/core/twi.h
@@ -0,0 +1,88 @@
+ /*
+  Copyright (c) 2017 Marcelo Henrique Moraes
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3.0 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+ */
+
+
+#ifndef TWI_H
+#define TWI_H
+
+#include "defines.h"
+
+extern uint8_t mode_twi;
+
+class TWI {
+private:
+	inline uint8_t status() { return TWSR & 0xF8;}
+	void address(uint8_t _address);
+public:
+	//initialize
+	TWI(uint8_t mode = MASTER, uint8_t speed = FAST);
+	~TWI() { PRR |= (1<<PRTWI); }
+	
+	//to communication
+	inline void start() {
+		TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+		while (!(TWCR & (1<<TWINT)));
+	}
+	inline void stop() { TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);}
+	void send(uint8_t data);
+	uint8_t recive(uint8_t ack = ACK);
+	
+	//to access data
+	inline uint8_t get() { return TWDR;	}
+	inline void put(uint8_t data) { TWDR = data; }
+	
+//to checks
+	
+	//in master mode
+
+	uint8_t ifstart();
+	
+	//in master and slave mode
+	
+	uint8_t ifack() {
+		uint8_t aux = status();
+#if mode_twi == MASTER
+		//master
+		return aux==0x18||aux==0x28;
+#elif mode_twi == SLAVE
+		//slave
+		return aux==0xB8;
+#endif	
+	};
+	
+	uint8_t ifnack() {
+		uint8_t aux = status();
+#if mode_twi == MASTER
+		//master
+		return aux==0x20||aux==30;
+#elif mode_twi == SLAVE
+		//slave
+		return aux==0xC0;
+#endif
+	};
+	
+	//in slave mode
+	inline uint8_t ifstop() { return status() == 0xA0; }	
+	inline uint8_t ifread() { return status()&0xA8;}
+	inline uint8_t ifwrite() { return status()==0x60;}
+
+	//to interrupts
+	void attach(VoidFuncPtr funct);
+	void detach();
+};
+
+
+
+#endif
