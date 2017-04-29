ee8046e053fea3b6ebcc4fa7ab4823683f9592cf
Marcelo Henrique Moraes <marceloh220@hotmail.com>
2017-04-29 17:40:17 -0300

    More updates

--- libraries/LiquidCrystal/src/LiquidCrystal.cpp
+++ libraries/LiquidCrystal/src/LiquidCrystal.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
 
-#include "LiquidCrystal.h"
+#include "LiquidCrystalMarcelino.h"
 
 // When the display powers up, it is configured as follows:
 //
